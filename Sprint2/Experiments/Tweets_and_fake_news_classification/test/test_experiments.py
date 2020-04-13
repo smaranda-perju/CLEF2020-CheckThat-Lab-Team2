76b1949aeb0474d583287ad4052fc08056424f05
Roxanica123 <irimia.andreea.roxana@gmail.com>
2020-04-13 22:01:25 +0300

    Added activities details

---
+++ Sprint2/Experiments/Tweets_and_fake_news_classification/test/test_experiments.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_load_data(self):
+        self.fail()
--- Sprint2/test_experiments.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-
-
-class Test(TestCase):
-    def test_load_data(self):
-        self.fail()
